56ca2c0554f2a73196df4e425b921c5075359ca3
dubliyu <cleonromero@gmail.com>
2019-05-30 13:28:23 -0400

    Added db

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import sys
-from PyQt5 import QtGui, 
+from PyQt5 import QtGui, QtWidgets
 
 # One Main Window, Multiple QStackedWidgets
 
-app = QtGui.QApplication([])
-
-window = QtGui.QWidget()
+app = QtWidgets.QApplication([])
+window = QtWidgets.QWidget()
 window.setGeometry(0, 0, 500, 300)
-window.setWindowTitle("Price Guessing Game")
+#window.setWindowTitle("Price Guessing Game")
 
 window.show()
-
+sys.exit(app.exec_())
 
